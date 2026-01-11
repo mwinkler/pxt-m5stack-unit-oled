28f6f6387b52ff1651932cba774e5c85c34afc07
Matthias Winkler <mwinkler@gmx.ch>
2026-01-11 16:02:29 +0100

    Fixes

--- main.ts
+++ main.ts
@@ -101,277 +101,254 @@ namespace m5oled {
         0x00,0x02,0x05,0x05,0x02,0x00,0x00,0x00 
         ];
 
-    /**
-     * Create Grove - Oled Display
-     */
-    //% blockId=grove_oled_create block="Create Oled Display"
-    export function createOled(): SH1107G
-    {
-        let oled = new SH1107G();
-        
-        oled.init();
-        oled.clearDisplay();
-        
-        return oled;
+    function sendData(data: number) {
+        let buf: Buffer = pins.createBuffer(2);
+        buf[0] = 0x40; // SeeedGrayOLED_Data_Mode
+        buf[1] = data;
+
+        pins.i2cWriteBuffer(0x3c, buf, false);
     }
 
-    export class SH1107G 
-    {
+    function sendCommand(cmd: number) {
+        let buf: Buffer = pins.createBuffer(2);
+        buf[0] = 0x80;  // SeeedGrayOLED_Command_Mode
+        buf[1] = cmd;
 
-        private sendData(data:number) {
-            let buf: Buffer = pins.createBuffer(2);
-            buf[0] = 0x40; // SeeedGrayOLED_Data_Mode
-            buf[1] = data;
+        pins.i2cWriteBuffer(0x3c, buf, false);
+    }
 
-            pins.i2cWriteBuffer(0x3c, buf, false);
-            // pins.i2cWriteBuffer(0x3c, buf);
-        }
+    /**
+     * Init OLED Display
+     */
+    //% blockId=m5oled_init block="Init OLED Display"
+    export function init() {
+        sendCommand(0xae);  // Display OFF
+        sendCommand(0x00);  // Set lower column address
+        sendCommand(0x10);  // Set higher column address
+        sendCommand(0x40);  // Set display start line
+        sendCommand(0xdc);  // Enable internal DC/DC converter
+        sendCommand(0x81);  // Set contrast
+        sendCommand(0x20);  // Mid contrast
+        sendCommand(0xa0);  // Segment remap
+        sendCommand(0xc8);  // COM output scan direction
+        sendCommand(0xa4);  // Set Entire Display ON
+        sendCommand(0xa6);  // Normal display
+        // sendCommand(0xd3);  // Set display offset
+        // sendCommand(0x20);  // Offset by 32 pixels
+        sendCommand(0xd5);  // Set clock divide
+        sendCommand(0x80);
+        sendCommand(0xd9);  // Set pre-charge period
+        sendCommand(0x1f);
+        sendCommand(0xdb);  // Set Vcomh
+        sendCommand(0x27);
+        sendCommand(0xaf);  // Display ON
+    }
 
-        private sendCommand(cmd:number) {
-            
-            let buf: Buffer = pins.createBuffer(2);
-            buf[0] = 0x80;  // SeeedGrayOLED_Command_Mode
-            buf[1] = cmd;
+    /**
+     * Set display position
+     * @param x which column to display, range from 0 to 63.
+     * @param y which row to display, range from 0 to 15.
+     */
+    //% blockId=m5oled_set_text_xy block="Set display position at x|%x|and y|%y"
+    //% x.min=0 x.max=63
+    //% y.min=0 y.max=15
+    export function setTextXY(x: number, y: number) {
+        x += 1;
+        let col_l: number = x % 16;
+        let col_h: number = (x / 16) + 0x10;
 
-            pins.i2cWriteBuffer(0x3c, buf, false);
-            // pins.i2cWriteBuffer(0x3c, buf);
-        }
+        sendCommand(0xb0 + y);
+        sendCommand(col_l);
+        sendCommand(col_h);
+    }
 
-        /**
-         * Init Grove - OLED Display
-         */
-        //% blockId=grove_oled_init block="%oled|Init Grove - OLED Display"
-        //% advanced=true
-        init() {
-            this.sendCommand(0xae);  // Display OFF 
-            this.sendCommand(0xd5);  // Set Dclk
-            this.sendCommand(0x50);  // 100Hz
-            this.sendCommand(0x20);  // Set row address
-            this.sendCommand(0x81);  // Set contrast control
-            this.sendCommand(0x80);  
-            this.sendCommand(0xa0);  // Segment remap
-            this.sendCommand(0xa4);  // Set Entire Display ON 
-            this.sendCommand(0xa6);  // Normal display
-            this.sendCommand(0xad);  // Set external VCC
-            this.sendCommand(0x80);
-            this.sendCommand(0xc0);  // Set Common scan direction
-            this.sendCommand(0xd9);  // Set phase leghth
-            this.sendCommand(0x1f);
-            this.sendCommand(0xdb);  // Set Vcomh voltage
-            this.sendCommand(0x27);
-            this.sendCommand(0xaf);  // Display ON
-            this.sendCommand(0xb0);
-            this.sendCommand(0x00);
-            this.sendCommand(0x11);
-        }
-
-        /**
-         * Set display position
-         * @param row which row to display, range from 0 to 15.
-         * @param col which col to display, range from 0 to 127.
-         */
-        //% blockId=grove_oled_set_text_xy block="%oled|Set display position at row|%row|and column|%col"
-        //% row.min=0 row.max=15
-        //% col.min=0 col.max=127
-        setTextXY(row:number, col:number) {
-            let col_l:number = col % 16;
-            let col_h:number = (col / 16)+0x10;
-
-            this.sendCommand(0xb0+row);
-            this.sendCommand(col_l);
-            this.sendCommand(col_h);
-        }
-
-        /**
-         * Clear display
-         */
-        //% blockId=grove_oled_clear_display block="%oled|Clear display"
-        clearDisplay() {
-            for(let i:number=0; i<16;i++){
-                this.sendCommand(0xb0+i);
-                this.sendCommand(0x0);
-                this.sendCommand(0x10);
-                for(let j=0; j<128;j++){ 
-                  this.sendData(0x00);  
-                }
+    /**
+     * Clear display
+     */
+    //% blockId=m5oled_clear_display block="Clear display"
+    export function clearDisplay() {
+        for(let i: number = 0; i < 16; i++){
+            sendCommand(0xb0 + i);
+            sendCommand(0x0);
+            sendCommand(0x10);
+            for(let j = 0; j < 128; j++){ 
+              sendData(0x00);  
             }
         }
+    }
 
-        private putChar(c:number) {
-            if (c < 32 || c > 127) {
-                c = 0; // space
-            }
-            else {
-                c = c-32;
-            }
-
-            for(let i=0; i< 8;i++){
-                this.sendData(oledFont[c*8+i]);
-            }
+    function putChar(c: number) {
+        if (c < 32 || c > 127) {
+            c = 0; // space
+        }
+        else {
+            c = c - 32;
         }
 
-        /**
-         * Display a string
-         * @param s a string to display.
-         */
-        //% blockId=grove_oled_put_string block="%oled|Display string |%s|"
-        putString(s:string) {
-            for (let n=0;n<s.length;n++){
-                this.putChar(s.charCodeAt(n));
+        for(let i = 0; i < 8; i++){
+            sendData(oledFont[c * 8 + i]);
+        }
+    }
+
+    /**
+     * Display a string
+     * @param s a string to display.
+     */
+    //% blockId=m5oled_put_string block="Display string |%s|"
+    export function putString(s: string) {
+        for (let n = 0; n < s.length; n++){
+            putChar(s.charCodeAt(n));
+        }
+    }
+
+    /**
+     * Display a integer number
+     * @param num a integer number to display.
+     */
+    //% blockId=m5oled_put_number block="Display integer number |%num|"
+    export function putNumber(num: number) {
+        putString(num.toString());
+    }
+
+    /**
+     * Display a bit map(32x32 max)
+     * @param x_start 
+     * @param y_start 
+     * @param row_number
+     * @param column_number
+     * @param bitmap
+     */
+    //% blockId=m5oled_draw_bitmap block="Draw bitmap start at row|%x_start|and column|%y_start|, size: row|%row_number|and column|%column_number|, bitmap:|%bitmap|"
+    //% x.min=0 x.max=7
+    //% y.min=0 y.max=127
+    //% row_number.min=0 row_number.max=4
+    //% column_number.min=0 column_number.max=32
+    //% advanced=true
+    export function drawBitmap(x_start: number, y_start: number, row_number: number, column_number: number, bitmap: number[]) {
+        let x_end = x_start + row_number;
+        let y_end = y_start + column_number;
+        if (x_end > 8) x_end = 8;
+        if (y_end > 128) y_end = 128;
+        let x_offset = 0, y_offset = 0;
+
+        for (let i = x_start; i < x_end; i++) {
+            y_offset = 0;
+            for (let j = y_start; j < y_end; j++) {
+                let temp_byte = bitmap[column_number * x_offset + y_offset];
+                y_offset++;
+                sendCommand(0xb0 + i);
+                sendCommand(j % 16);
+                sendCommand(j / 16 + 0x10);
+                sendData(temp_byte);
             }
+            x_offset++;
+        }
+    }
+
+    export function drawPixel(x: number, y: number, data: number) {
+        if (x < 0) x = 0;
+        else if (x > 63) x = 63;
+        if (y < 0) y = 0;
+        else if (y > 127) y = 127;
+        if (data < 0) data = 0;
+        else if (data > 255) data = 255;
+
+        setTextXY(x, y / 8);
+        sendData(data);
+    }
+
+    /**
+     * Draw a horizontal line
+     * @param x  
+     * @param y
+     * @param len
+     */
+    //% blockId=m5oled_draw_hline block="Draw horizontal line start at x|%x|and y|%y|, length|%len|"
+    //% x.min=0 x.max=63
+    //% y.min=0 y.max=127
+    //% len.min=1 len.max=64
+    export function drawHLine(x: number, y: number, len: number) {
+        let x_max = x + len;
+        if (x_max > 128) x_max = 128;
+        for (let i = x; i < x_max; i++) {
+            drawPixel(i, y, 0x01 << (y % 8));
+        }
+    }
+
+    /**
+     * Draw a vertical line
+     * @param x  
+     * @param y
+     * @param len
+     */
+    //% blockId=m5oled_draw_vline block="Draw vertical line start at x|%x|and y|%y|, length|%len|"
+    //% x.min=0 x.max=63
+    //% y.min=0 y.max=127
+    //% len.min=1 len.max=128
+    export function drawVLine(x: number, y: number, len: number) {
+        let y_min = 0, y_max = 0;
+        y_min = Math.floor((y / 8));
+        y_max = y + len;
+        y_min = y_min * 8;
+        if (y_max > 128) y_max = 128;
+        while ((y_max % 8) != 0) {
+            y_max++;
         }
 
-        /**
-         * Display a integer number
-         * @param num a integer number to display.
-         */
-        //% blockId=grove_oled_put_number block="%oled|Display integer number |%num|"
-        putNumber(num:number) {
-            this.putString(num.toString());
+        let last_bit = 0xff;
+        for (let i = 0; i < (y_max - y - len); i++) {
+            last_bit = last_bit - (0x01 << (7 - i));
+        }
+        let first_bit = 0xff;
+        for (let i = 0; i < (y - y_min); i++) {
+            first_bit = first_bit - (0x01 << i);
+        }
+        
+        if (y_max - y_min > 16) {
+            drawPixel(x, y_min, first_bit);
+            for (let i = y_min + 8; i < y_max - 8; i = i + 8){
+                drawPixel(x, i, 0xff);
+            }
+            drawPixel(x, y_max - 1, last_bit);
+        }
+        else if (y_max - y_min == 16) {
+            drawPixel(x, y_min, first_bit);
+            drawPixel(x, y_max - 1, last_bit);
+        }
+        else {
+            drawPixel(x, y_min, (first_bit & last_bit));
+        }
+    }
+
+    /**
+     * Draw a rectangle
+     * @param x1  
+     * @param y1
+     * @param x2
+     * @param y2
+     */
+    //% blockId=m5oled_draw_rec block="Draw a rectangle start at x|%x1|and y|%y1|, end at x|%x2|and y|%y2|"
+    //% x1.min=0 x1.max=63
+    //% y1.min=0 y1.max=127
+    //% x2.min=0 x2.max=63
+    //% y2.min=0 y2.max=127
+    export function drawRec(x1: number, y1: number, x2: number, y2: number) {
+        let temp = 0;
+        if (x2 < x1) {
+            temp = x2;
+            x2 = x1;
+            x1 = temp;
+        }
+        if (y2 < y1) {
+            temp = y2;
+            y2 = y1;
+            y1 = temp;
         }
 
-        /**
-         * Display a bit map(32x32 max)
-         * @param x_start 
-         * @param y_start 
-         * @param row_number
-         * @param column_number
-         * @param bitmap
-         */
-        //% blockId=grove_oled_draw_bitmap block="%oled|Draw bitmap start at row|%x_start|and column|%y_start|, size: row|%row_number|and column|%column_number|, bitmap:|%bitmap|"
-        //% x.min=0 x.max=15
-        //% y.min=0 y.max=127
-        //% row_number.min=0 row_number.max=4
-        //% column_number.min=0 column_number.max=32
-        //% advanced=true
-        drawBitmap(x_start:number,y_start:number,row_number:number,column_number:number,bitmap:number[]) {
-            let x_end = x_start+row_number;
-            let y_end = y_start+column_number;
-            if (x_end > 16) x_end = 16;
-            if (y_end > 128) y_end = 128;
-            let x_offset = 0, y_offset = 0;
-
-            for (let i=x_start; i<x_end; i++) {
-                y_offset = 0;
-                for (let j=y_start; j<y_end; j++) {
-                    let temp_byte = bitmap[column_number*x_offset+y_offset];
-                    y_offset++;
-                    this.sendCommand(0xb0+i);
-                    this.sendCommand(j % 16);
-                    this.sendCommand(j/16 + 0x10);
-                    this.sendData(temp_byte);
-                }
-                x_offset ++ ;
-            }
-            
-        }
-
-        private drawPixel(x: number, y:number, data:number) {
-            if (x<0) x = 0;
-            else if (x>127) x = 127;
-            if (y<0) y = 0;
-            else if (y>127) y = 127;
-            if (data < 0) data = 0;
-            else if (data > 255) data = 255;
-
-            this.setTextXY(x/8,y);
-            this.sendData(data);
-        }
-
-        /**
-         * Draw a horizontal line
-         * @param x  
-         * @param y
-         * @param len
-         */
-        //% blockId=grove_oled_draw_hline block="%oled|Draw horizontal line start at x|%x|and y|%y|, length|%len|"
-        //% y.min=0 y.max=127
-        //% x.min=0 x.max=127
-        //% len.min=1 len.max=128
-        drawHLine(x: number, y: number, len: number) {
-            let y_max = y + len;
-            if (y_max > 128) y_max = 128;
-            for (let i=y;i<y_max;i++) {
-                this.drawPixel(x,i,0x01<<(x%8));
-            }
-        }
-
-        /**
-         * Draw a vertical line
-         * @param x  
-         * @param y
-         * @param len
-         */
-        //% blockId=grove_oled_draw_vline block="%oled|Draw vertical line start at x|%x|and y|%y|, length|%len|"
-        //% y.min=0 y.max=127
-        //% x.min=0 x.max=127
-        //% len.min=1 len.max=128
-        drawVLine(x: number, y: number, len: number) {
-            let x_min = 0, x_max = 0;
-            x_min = Math.floor((x / 8));
-            x_max = x + len;
-            x_min = x_min * 8;
-            if (x_max > 128) x_max = 128;
-            while ((x_max % 8) != 0) {
-                x_max++;
-            }
-
-            let last_bit = 0xff;
-            for (let i=0;i<(x_max-x-len);i++) {
-                last_bit = last_bit - (0x01<<(7-i));
-            }
-            let first_bit = 0xff;
-            for (let i=0;i<(x-x_min);i++) {
-                first_bit = first_bit - (0x01<<i);
-            }
-            
-            if (x_max - x_min > 16) {
-                this.drawPixel(x_min,y,first_bit);
-                for (let i=x_min+8;i<x_max-8;i=i+8){
-                    this.drawPixel(i,y,0xff);
-                }
-                this.drawPixel(x_max-1,y,last_bit);
-            }
-            else if (x_max - x_min == 16) {
-                this.drawPixel(x_min,y,first_bit);
-                this.drawPixel(x_max-1,y,last_bit);
-            }
-            else {
-                this.drawPixel(x_min,y,(first_bit & last_bit));
-            }
-        }
-
-        /**
-         * Draw a rectangle
-         * @param x1  
-         * @param y1
-         * @param x2
-         * @param y2
-         */
-        //% blockId=grove_oled_draw_rec block="%oled|Draw a rectangle start at x|%x1|and y|%y1|, end at x|%x2|and y|%y2|"
-        //% y1.min=0 y1.max=127
-        //% x1.min=0 x1.max=127
-        //% y2.min=0 y2.max=127
-        //% x2.min=0 x2.max=127
-        drawRec(x1: number, y1: number, x2:number, y2:number) {
-            let temp = 0;
-            if (y2<y1) {
-                temp = y2;
-                y2 = y1;
-                y1 = temp;
-            }
-            if (x2<x1) {
-                temp = x2;
-                x2 = x1;
-                x1 = temp;
-            }
-
-            this.drawHLine(x1,y1,y2-y1+1);
-            this.drawHLine(x2,y1,y2-y1+1);
-            this.drawVLine(x1,y1,x2-x1+1);
-            this.drawVLine(x1,y2,x2-x1+1);
-
-        }
+        drawHLine(x1, y1, x2 - x1 + 1);
+        drawHLine(x1, y2, x2 - x1 + 1);
+        drawVLine(x1, y1, y2 - y1 + 1);
+        drawVLine(x2, y1, y2 - y1 + 1);
     }
 
 }
